e12291e4f3dc5f319f367a1947696af17e9d5e87
Jack Xu [mac] <projectbyjx@gmail.com>
2019-12-28 17:36:38 -0500

    mend

---
+++ main/src/Helper/task_helper.h
@@ -0,0 +1,27 @@
+#ifndef TASK_HELPER_H_
+#define TASK_HELPER_H_
+
+typedef struct
+{
+	unsigned long t_ms;
+	unsigned int  interval_ms;
+
+	void init(unsigned int interval)
+	{
+		interval_ms = interval;
+	}
+
+	bool onTick(void)
+	{
+		bool result = false;
+		unsigned long c_time = millis();
+		if (c_time - t_ms > interval_ms)
+		{
+			t_ms = c_time;
+			result = true;
+		}
+		return result;
+	}
+} LoopWatchDog_t;
+
+#endif //TASK_HELPER_H_
